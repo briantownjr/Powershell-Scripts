fd8e7d4ac8053800764afe4b90378dda47ac8697
briantownjr <thelordymir@gmail.com>
2015-08-07 10:43:00 -0400

    Uploading 2 new scripts

---
+++ add_AD_users.ps1
@@ -0,0 +1,10 @@
+﻿#Import CSV of users and assign them to designated groups
+#EXAMPLE of CSV:
+#DisplayName,group
+#testguy,testgroup
+Import-Csv C:\Users\Administrator\Desktop\people.csv | %{
+$name = $_.DisplayName
+$SamAccountName = Get-ADUser -Filter{ name -like $name }|`
+
+Add-ADGroupMember -Identity $_.group -Member $SamAccountName
+}
---
+++ rpc_Ports.ps1
@@ -0,0 +1,34 @@
+﻿#Sets RPC port range
+
+$port_range = '5000-5300'
+$ports_available = 'Y'
+$use_ports = 'Y'
+
+if (Test-Path HKLM:\SOFTWARE\Microsoft\Rpc\Internet){
+    if (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Rpc\Internet -Name Ports -ErrorAction SilentlyContinue){
+        Set-ItemProperty HKLM:\Software\Microsoft\Rpc\Internet -Name Ports -Value $port_range -Type MultiString > $null
+    }
+    else {New-ItemProperty HKLM:\Software\Microsoft\Rpc\Internet -Name Ports -Value $port_range -Type MultiString > $null
+    }
+
+
+    if (Get-ItemProperty HKLM:\Software\Microsoft\Rpc\Internet -Name PortsInternetAvailable -ErrorAction SilentlyContinue){
+        Set-ItemProperty HKLM:\Software\Microsoft\Rpc\Internet -Name PortsInternetAvailable -Value $ports_available -Type String > $null
+    }
+    else {New-ItemProperty HKLM:\Software\Microsoft\Rpc\Internet -Name PortsInternetAvailable -Value 'Y' -Type String > $null
+    }
+
+    if (Get-ItemProperty HKLM:\Software\Microsoft\Rpc\Internet -Name UseInternetPorts -ErrorAction SilentlyContinue){
+        Set-ItemProperty HKLM:\Software\Microsoft\Rpc\Internet -Name UseInternetPorts -Value $use_ports -Type String > $null
+    }
+    else {New-ItemProperty HKLM:\Software\Microsoft\Rpc\Internet -Name UseInternetPorts -Value 'Y' -Type String > $null
+    }
+}
+
+else { New-Item -Path HKLM:\Software\Microsoft\Rpc -Name Internet 
+    New-ItemProperty HKLM:\Software\Microsoft\Rpc\Internet -Name Ports -Value $port_range -Type MultiString > $null
+    New-ItemProperty HKLM:\Software\Microsoft\Rpc\Internet -Name PortsInternetAvailable -Value 'Y' -Type String > $null
+    New-ItemProperty HKLM:\Software\Microsoft\Rpc\Internet -Name UseInternetPorts -Value 'Y' -Type String > $null
+    break
+}
+
